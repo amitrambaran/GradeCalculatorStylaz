bd479f345067d5c4d882729b66ae72df9c7f2487
jayshah1232 <jayshah8630@hotmail.com>
2017-07-19 12:46:56 -0400

    - Addit Git_test activity to test pushes and pulls

---
+++ app/src/main/java/com/example/jayshah/gradecalculatorstylaz/Git_test.java
@@ -0,0 +1,13 @@
+package com.example.jayshah.gradecalculatorstylaz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Git_test extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_git_test);
+    }
+}
