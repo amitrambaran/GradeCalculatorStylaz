c2ccbd42a004513c8b7a21daff24f4fdf457db8a
jayshah1232 <jayshah8630@hotmail.com>
2017-07-19 03:48:47 -0400

    - Added college_selection scroll page

---
+++ app/src/main/java/com/example/jayshah/gradecalculatorstylaz/college_selection.java
@@ -0,0 +1,13 @@
+package com.example.jayshah.gradecalculatorstylaz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class college_selection extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_college_selection);
+    }
+}
