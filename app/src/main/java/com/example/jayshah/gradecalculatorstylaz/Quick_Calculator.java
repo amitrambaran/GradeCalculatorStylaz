dddca6aaf50e37c07fafd190a2352c87721c5e06
jayshah1232 <jayshah8630@hotmail.com>
2017-07-20 04:03:08 -0400

    - Addit Git_test activity to test pushes and pulls

---
+++ app/src/main/java/com/example/jayshah/gradecalculatorstylaz/Quick_Calculator.java
@@ -0,0 +1,13 @@
+package com.example.jayshah.gradecalculatorstylaz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Quick_Calculator extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quick__calculator);
+    }
+}
